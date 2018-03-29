780a5e45c9a8089acaf11f972e9d56b6d9af8fd9
Sergio Del Castillo <serdelcas1990@gmail.com>
2018-03-29 12:06:46 +0200

    Finished and translated to spanish

--- __manifest__.py
+++ __manifest__.py
@@ -7,25 +7,26 @@
 
     'description': """
         This module give you a better way to make internal transfers between journals
-            - Allow to your users make internal transfers since pos.session kanban
-            - Give you a new menu on Account/Adviser/Internal transfers to show the internal transfers
+        - Allow to your users make internal transfers since pos.session kanban
+        - Give you a new menu on Account/Adviser/Internal transfers to show the internal transfers
 
 
-            IMAGES: 
+        IMAGES: 
 
-            The button that appears on pos.session kanban to make internal transfers
-            [[https://github.com/serCliff/account_internal_moves/10.0/img/pos_session.png|alt=pos_session]]
-
-
-            The account.payment form to make a internal transfer
-            [[https://github.com/serCliff/account_internal_moves/10.0/img/pos_session.png|alt=pos_session]]
+        The button that appears on pos.session kanban to make internal transfers
+        https://github.com/serCliff/account_internal_moves/blob/10.0/img/pos_session.png
 
 
-            STILL MISSING
-            - Not transated
-            - Without seccurity groups
+        The account.payment form to make a internal transfer
+        https://github.com/serCliff/account_internal_moves/blob/10.0/img/account_payment.png
 
-            git: https://github.com/serCliff/account_internal_moves
+
+        STILL MISSING
+        
+        - Not transated
+        - Without seccurity groups
+
+        git: https://github.com/serCliff/account_internal_moves
     """,
 
 
